4def900f3ac4afe16f523f5707cf236264bd5e22
RWEERASOORIYA <RWEERASOORIYA@CMLRWEERASOORIY.Virtusa.com>
2019-06-27 14:27:28 +0530

    Initial commit

---
+++ hystrix-student-service/src/main/java/com/poc/school/student/HystrixStudentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poc.school.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixStudentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixStudentServiceApplication.class, args);
+	}
+
+}
