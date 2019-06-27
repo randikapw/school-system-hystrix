44e823e27208c6f8ed877431348a11f5c41959b5
randikapw <randikapw@gmail.com>
2019-06-27 17:10:31 +0530

    Create School Service

---
+++ hystrix-school-service/src/main/java/com/poc/school/HystrixSchoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.poc.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixSchoolServiceApplication.class, args);
+	}
+
+}
