3670cc60d341a22a2c1e5388203077f5afb74de2
randikapw <randikapw@gmail.com>
2019-06-28 10:52:05 +0530

    *Map uri with variables

--- hystrix-school-service/src/main/java/com/poc/school/services/StudentServiceDeligate.java
+++ hystrix-school-service/src/main/java/com/poc/school/services/StudentServiceDeligate.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -17,7 +18,17 @@ public class StudentServiceDeligate {
 	
 	public String getStudents(String schoolName) {
 		
-		return null;
+		//Setting up Uri builder with query params
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(URI_GET_STUDENTS)
+				//set query params
+				.queryParam(PARAM_KEY_GET_STUDENTS, schoolName);
+		
+		// **Use below link to see how to add path variables to UriComponentBuilder
+		// https://stackoverflow.com/questions/35998790/resttemplate-how-to-send-url-and-query-parameters-together?answertab=votes#tab-top
+		
+		String result = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, String.class).getBody();
+		
+		return result;
 	}
 	
 }
