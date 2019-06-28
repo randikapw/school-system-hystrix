a63d300104b2a110a88d891c116afed9a2d9c154
RWEERASOORIYA <RWEERASOORIYA@CMLRWEERASOORIY.Virtusa.com>
2019-06-28 11:11:41 +0530

    fixed constant values.

--- hystrix-school-service/src/main/java/com/poc/school/controller/SchoolServiceController.java
+++ hystrix-school-service/src/main/java/com/poc/school/controller/SchoolServiceController.java
@@ -10,14 +10,15 @@ import com.poc.school.services.StudentServiceDeligate;
 @RestController
 public class SchoolServiceController {
 	
-	private static final String PATH_VAR_SCHOOL_NAME = "{schoolName}";
+	private static final String PATH_VAR_SCHOOL_NAME = "schoolName";
 	private static final String URI_GET_SCHOOLS = "/schools/" + PATH_VAR_SCHOOL_NAME;
+	private static final String URI_GET_STUDENTS_OF_SCHOOL = "/schools/{" + PATH_VAR_SCHOOL_NAME + "}/students";
 	
 	@Autowired
 	StudentServiceDeligate studentServiceDeligate;
 
-	@GetMapping(URI_GET_SCHOOLS)
-	public String getStudentsOfSchool(@PathVariable(name=PATH_VAR_SCHOOL_NAME) String schoolName) {
+	@GetMapping(URI_GET_STUDENTS_OF_SCHOOL)
+	public String getStudentsOfSchool(@PathVariable(value=PATH_VAR_SCHOOL_NAME) String schoolName) {
 		return studentServiceDeligate.getStudents(schoolName);
 	} 
 }
